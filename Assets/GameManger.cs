ca9b474936916a162694a4d7ef26cdc87628d639
1946625919@qq.com <lhw870926>
2018-11-16 10:14:55 +0800

    1
    1

---
+++ Assets/GameManger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManger : MonoBehaviour {
+
+	public void OnStartGame(string ScneneName){
+		SceneManager.LoadScene (ScneneName);
+	}
+
+	public void QuitGame(){
+		Application.Quit ();
+	}
+}
